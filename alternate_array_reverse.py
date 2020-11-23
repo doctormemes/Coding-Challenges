9feaebdcfd22490c52473356918fbe279da3bd31
Doc <19937034+doctormemes@users.noreply.github.com>
2020-11-23 10:58:56 -0600

    Create alternate_array_reverse.py
    alternate way of reversing an array (well, technically it's a list)

---
+++ alternate_array_reverse.py
@@ -0,0 +1,12 @@
+# alternate way to reverse the array (actually it's a list) without numpy
+
+# function for reversing
+def reverse_list_array(array_list): # array_list param to keep from initializing variable
+    for i in range(len(array_list)-1): # loop through the range of array_list's length (-1 because len starts at 1)
+        array_list.append(array_list[i]) # append array_list with i's corresponding value (appends it to the end of array_list so nbd)
+        array_list.pop(i) # pops (removes) item at the index location of i, preventing duplicates
+    return array_list # return list
+
+this_is_a_list_array = ["H", "e", "l", "l", "o", "!"]
+
+print(reverse_list_array(this_is_a_list_array))
