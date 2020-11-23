935341b71663ce7b49c35254e84b3de3809ef9e4
Doc <19937034+doctormemes@users.noreply.github.com>
2020-11-23 08:30:57 -0600

    Add files via upload

--- reverseArrayChallenge.py
+++ reverseArrayChallenge.py
@@ -8,13 +8,13 @@ import numpy as np
     # big daddy string to get things done
 def string_to_array_to_reversed_array():
         # function for handling user input
-    def user_input_function(user_input): # user_input to take in the input when called, non-keyword parameter string_arg to take in the string from user_input as a list
-        user_input = list(user_input.replace(" ", "")) # user_input takes in param string as a list, while within the list user_input.replace trims any white space from user input
-        return np.array(user_input, order = 'C') # return user_input list as array in contiguous order
+    def user_input_function(user_input): # user_input to take in the input when called
+        user_input = list(user_input.replace(" ", "")) # user_input is reassigned it's input string as a list, while user_input.replace trims any white space out
+        return np.array(user_input, order = 'C') # return user_input list as an array in contiguous order to array_arg parameter in reverse_array function
 
         # function for reversing the array
-    def reverse_array(array_arg): # array_arg to take in the np.array values.
-        return np.flipud(array_arg) # FLIPS THE ARRAY, DOESN'T REVERSE IT. YOU SEE THE FLIPUD part right?. NOTHING WAS SAID ABOUT FLIPPING.
+    def reverse_array(array_arg): # array_arg takes in the return from user_input_function
+        return np.flipud(array_arg) # FLIPS THE ARRAY, DOESN'T REVERSE IT. YOU SEE THE .flipud part right?. NOTHING WAS SAID ABOUT FLIPPING. ONLY REVERSING.
 
     # decided to just use a variable here to store the results for printing
     result = reverse_array(user_input_function(input("Please type in a single word as a string: ")))
